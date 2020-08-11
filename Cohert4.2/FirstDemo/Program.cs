f4029b8bb81d21d8d4b213c0c8a7861556ea68a6
dhmec <dharmoradiya@gmail.com>
2020-08-11 12:25:16 -0600

    Name and Age input and prints our your name and age together;

--- Cohert4.2/FirstDemo/Program.cs
+++ Cohert4.2/FirstDemo/Program.cs
@@ -6,7 +6,13 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name : ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your age : ");
+            int age = int.Parse(Console.ReadLine());
+  
+            Console.WriteLine("Hello, " + name + " You are " + age + " old");
         }
     }
 }
